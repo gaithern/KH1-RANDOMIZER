cf1fff30f5c8be8b2b5efba5b7e27de54d6183eb
gaithern <gaithern@gmail.com>
2026-06-04 20:01:54 -0500

    Does this work?

--- mod/scripts/1fm4By3.lua
+++ mod/scripts/1fm4By3.lua
@@ -3,18 +3,19 @@ LUAGUI_AUTH = "denhonator (edited by deathofall84)"
 LUAGUI_DESC = "Changes aspect ratio. Visit display settings to apply"
 
 local seed_vars = require("seed_vars")
+local ok = false
 
 function _OnInit()
 	if GAME_ID == 0xAF71841E and ENGINE_TYPE == "BACKEND" then
 		require("VersionCheck")
-		canExecute = canExecute and seed_vars.settings["four_by_three"]
+		ok = canExecute and seed_vars.settings["four_by_three"]
 	else
 		ConsolePrint("KH1 not detected, not running script")
 	end
 end
 
 function _OnFrame()
-	if canExecute then
+	if ok then
 		WriteFloat(height, 12.0) -- Sets aspect ratio to 16:12 = 4:3
 	end
 end
