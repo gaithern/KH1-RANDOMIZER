6642cb623f3eacfc61470b5c9d14f96720f2e579
gaithern <gaithern@gmail.com>
2026-06-08 19:37:00 -0500

    btltbl.bin is not loaded immediately, have to account for that

--- mod/scripts/1fmRandoHandleKeybladeStats.lua
+++ mod/scripts/1fmRandoHandleKeybladeStats.lua
@@ -2,8 +2,9 @@ LUAGUI_NAME = "1fmRandoHandleKeybladeStats"
 LUAGUI_AUTH = "Gicu"
 LUAGUI_DESC = "Kingdom Hearts 1FM Handle Keyblade Stats"
 
-local kh1_lua_library = require("kh1_lua_library")
 local seed_vars = require("seed_vars")
+
+local keyblade_stats_written = false
 
 local init_offsets = {
     ["STR"] = 0x9528,
@@ -24,13 +25,15 @@ end
 function _OnInit()
     if GAME_ID == 0xAF71841E and ENGINE_TYPE == "BACKEND" then
         require("VersionCheck")
-        if canExecute then
-            write_keyblade_stats()
-        end
     else
         ConsolePrint("KH1 not detected, not running script")
     end
 end
 
 function _OnFrame()
+    if not canExecute then return end
+    if keyblade_stats_written then return end
+    if ReadByte(jumpHeights - 0xAC) == 0x0 then return end -- btltbl.bin not loaded yet
+    write_keyblade_stats()
+    keyblade_stats_written = true
 end
