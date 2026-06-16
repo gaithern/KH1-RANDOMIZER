504fde1c0acac4dd921ed98b093cdbcb74da968f
gaithern <gaithern@gmail.com>
2026-06-16 15:06:11 -0500

    Typo to fix in bouncing spot

--- asm/remastered/po08.ard/UK_po08_ardc.evdl.asm
+++ asm/remastered/po08.ard/UK_po08_ardc.evdl.asm
@@ -991,7 +991,7 @@
 ;  07000009  push            0x7             
 ;  00000009  push            0x0             
 ;  01000009  push            0x1             
-  03000018  syscall         3                 ; Set_window_position
+;  03000018  syscall         3                 ; Set_window_position
   07000009  push            0x7             
   00000018  syscall         0                 ; Open_window
   07000009  push            0x7             
