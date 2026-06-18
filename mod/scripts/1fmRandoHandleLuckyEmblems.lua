e6d7ee0a30bc3dc339f8c5e9be5fa19ab6b33ea1
gaithern <gaithern@gmail.com>
2026-06-18 07:57:22 -0500

    Fix EOTW and Door opening too soon when lucky emblems are off

--- mod/scripts/1fmRandoHandleLuckyEmblems.lua
+++ mod/scripts/1fmRandoHandleLuckyEmblems.lua
@@ -5,13 +5,13 @@ LUAGUI_DESC = "Kingdom Hearts 1FM Unlock End of the World or Final Door Key with
 local seed_vars = require("seed_vars")
 
 local function unlock_eotw()
-    if ReadByte(inventory + 237) >= seed_vars["settings"]["required_lucky_emblems_eotw"] and ReadByte(inventory + 168) == 0 then
+    if ReadByte(inventory + 237) >= seed_vars["settings"]["required_lucky_emblems_eotw"] and seed_vars["settings"]["end_of_the_world_unlock"] == "lucky_emblems" and ReadByte(inventory + 168) == 0 then
         WriteByte(inventory + 168, 1)
     end
 end
 
 local function unlock_door()
-    if ReadByte(inventory + 237) >= seed_vars["settings"]["required_lucky_emblems_door"] and ReadByte(inventory + 9) == 0 then
+    if ReadByte(inventory + 237) >= seed_vars["settings"]["required_lucky_emblems_door"] and seed_vars["settings"]["final_rest_door_key"] == "lucky_emblems" and ReadByte(inventory + 9) == 0 then
         WriteByte(inventory + 9, 1)
     end
 end
