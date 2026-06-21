a862c529d77c3faee04b3fe16ac59080e2c135d5
gaithern <gaithern@gmail.com>
2026-06-21 09:05:10 -0500

    Fix Synth item names

--- mod/scripts/1fmRandoWriteSynthesisItemNames.lua
+++ mod/scripts/1fmRandoWriteSynthesisItemNames.lua
@@ -5,12 +5,22 @@ LUAGUI_DESC = "Kingdom Hearts 1FM Randomizer Write Synthesis Item Names"
 
 local seed_vars = require("seed_vars")
 
+local SYNTH_ITEM_NAME_MESSAGE_INDEX = 425
+
 local function write_synth_item_name()
     local synth_item_selected_index = ReadByte(synthItemSelected)
-    for k,v in ipairs(seed_vars["settings"]["synthesis_item_name_byte_arrays"][synth_item_selected_index+1]) do
-        WriteByte(language + 0x2593 + k - 1, v)
+    local tbl_start = ReadInt(UK_Word + 0x10)
+    local str_base = ReadInt(UK_Word + 0x14)
+    local msg_offset = ReadShort(UK_Word + tbl_start + SYNTH_ITEM_NAME_MESSAGE_INDEX * 2)
+    local next_msg_offset = ReadShort(UK_Word + tbl_start + (SYNTH_ITEM_NAME_MESSAGE_INDEX + 1) * 2)
+    local max_name_length = next_msg_offset - msg_offset - 1 -- leave room for the null terminator
+    local msg_address = UK_Word + str_base + msg_offset
+    local name_bytes = seed_vars["settings"]["synthesis_item_name_byte_arrays"][synth_item_selected_index+1]
+    local write_length = math.min(#name_bytes, max_name_length)
+    for k = 1, write_length do
+        WriteByte(msg_address + k - 1, name_bytes[k])
     end
-    WriteByte(language + 0x2593 + #seed_vars["settings"]["synthesis_item_name_byte_arrays"][synth_item_selected_index+1], 0)
+    WriteByte(msg_address + write_length, 0)
 end
 
 function _OnInit()
