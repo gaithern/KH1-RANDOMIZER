b663cde48baed877febfed005b8ffb22ee9d7f5d
gaithern <gaithern@gmail.com>
2026-09-03 15:08:56 -0500

    Maybe this is correct

--- mod/scripts/1fmRandoClient.lua
+++ mod/scripts/1fmRandoClient.lua
@@ -256,7 +256,7 @@ local function connect(server, slot, password)
                 elseif receiver_id == ap:get_player_number() and kh1_lua_library.contains(remote_location_ids, location_id) then
                     line1 = "Obtained " .. item_name
                     line2 = nil
-                elseif receiver_id == ap:get_player_number() and sender_id == -1 then
+                elseif receiver_id == ap:get_player_number() and location_id == -1 then
                     line1 = "Obtained " .. item_name
                     line2 = "from server"
                 end
